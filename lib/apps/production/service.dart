c584e2a182118f92edeb170bf89bafd8aff67269
Brayan Mota <brayan@palmas.to.gov.br>
2023-06-20 18:39:36 -0300

    Adicionando um endpoint para pesquisar apenas um filme

--- lib/apps/production/service.dart
+++ lib/apps/production/service.dart
@@ -5,16 +5,21 @@ import 'package:http/http.dart';
 import 'models.dart';
 
 class ProductionService {
-  final String apiKey = "32e129c3";
-  final url = "https://www.omdbapi.com/?apikey=32e129c3&type=movie&s=";
+  final String apiKey;
+  final String url;
   final List<ProductionModel> _productionList = <ProductionModel>[];
 
-  Future<List<ProductionModel>> testeRequisicao(String search) async {
+  ProductionService({
+    this.apiKey = "32e129c3",
+    this.url = "https://www.omdbapi.com/",
+  });
+
+  Future<List<ProductionModel>> getProductions(String search) async {
     try {
       _productionList.clear();
-      final uri = this.url + search;
-      final url = Uri.parse(uri);
-      final response = await get(url);
+      final String urlFormated = '$url?apikey=$apiKey&type=movie&s=$search';
+      final uri = Uri.parse(urlFormated);
+      final response = await get(uri);
       if (response.statusCode == 200) {
         final body = jsonDecode(response.body);
         if (body["Response"] == "True") {
@@ -29,5 +34,21 @@ class ProductionService {
       Exception(error.toString());
     }
     return _productionList;
+  }
+
+  Future<ProductionModel> getProduction(String imdbID) async {
+    ProductionModel productionModel = ProductionModel();
+    try {
+      final String urlFormated = '$url?apikey=$apiKey&type=movie&i=$imdbID';
+      final uri = Uri.parse(urlFormated);
+      final response = await get(uri);
+      if (response.statusCode == 200) {
+        final body = jsonDecode(response.body);
+        productionModel = ProductionModel.fromJson(body);
+      }
+    } catch (error) {
+      Exception(error.toString());
+    }
+    return productionModel;
   }
 }
