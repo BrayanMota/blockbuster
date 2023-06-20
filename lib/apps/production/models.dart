2e5fdf450478e913d18359ed71c77c41a3af430f
Brayan Mota <brayan@palmas.to.gov.br>
2023-06-20 18:38:54 -0300

    ✨ Refatorando as models de Production

--- lib/apps/production/models.dart
+++ lib/apps/production/models.dart
@@ -1,56 +1,56 @@
 class ProductionModel {
   String title;
-  String? year;
-  String? rated;
-  String? released;
-  String? runtime;
-  String? genre;
-  String? director;
-  String? writer;
-  String? actors;
-  String? plot;
-  String? language;
-  String? country;
-  String? awards;
+  String year;
+  String rated;
+  String released;
+  String runtime;
+  String genre;
+  String director;
+  String writer;
+  String actors;
+  String plot;
+  String language;
+  String country;
+  String awards;
   String poster;
   List<Ratings>? ratings;
-  String? metascore;
-  String? imdbRating;
-  String? imdbVotes;
-  String? imdbID;
-  String? type;
-  String? dVD;
-  String? boxOffice;
-  String? production;
-  String? website;
-  String? response;
+  String metascore;
+  String imdbRating;
+  String imdbVotes;
+  String imdbID;
+  String type;
+  String dvd;
+  String boxOffice;
+  String production;
+  String website;
+  String response;
 
   ProductionModel({
     this.title = '',
-    this.year,
-    this.rated,
-    this.released,
-    this.runtime,
-    this.genre,
-    this.director,
-    this.writer,
-    this.actors,
-    this.plot,
-    this.language,
-    this.country,
-    this.awards,
+    this.year = '',
+    this.rated = '',
+    this.released = '',
+    this.runtime = '',
+    this.genre = '',
+    this.director = '',
+    this.writer = '',
+    this.actors = '',
+    this.plot = '',
+    this.language = '',
+    this.country = '',
+    this.awards = '',
     this.poster = '',
     this.ratings,
-    this.metascore,
-    this.imdbRating,
-    this.imdbVotes,
-    this.imdbID,
-    this.type,
-    this.dVD,
-    this.boxOffice,
-    this.production,
-    this.website,
-    this.response,
+    this.metascore = '',
+    this.imdbRating = '',
+    this.imdbVotes = '',
+    this.imdbID = '',
+    this.type = '',
+    this.dvd = '',
+    this.boxOffice = '',
+    this.production = '',
+    this.website = '',
+    this.response = '',
   });
 
   factory ProductionModel.fromJson(Map<String, dynamic> json) {
@@ -61,36 +61,31 @@ class ProductionModel {
         imdbID: json.containsKey("imdbID") ? json["imdbID"] ?? "" : "",
         type: json.containsKey("Type") ? json["Type"] ?? "" : "",
         poster: json.containsKey("Poster") ? json["Poster"] ?? "" : "",
+        rated: json.containsKey("Rated") ? json["Rated"] ?? "" : "",
+        released: json.containsKey("Released") ? json["Released"] ?? "" : "",
+        runtime: json.containsKey("Runtime") ? json["Runtime"] ?? "" : "",
+        genre: json.containsKey("Genre") ? json["Genre"] ?? "" : "",
+        director: json.containsKey("Director") ? json["Director"] ?? "" : "",
+        writer: json.containsKey("Writer") ? json["Writer"] ?? "" : "",
+        actors: json.containsKey("Actors") ? json["Actors"] ?? "" : "",
+        plot: json.containsKey("Plot") ? json["Plot"] ?? "" : "",
+        language: json.containsKey("Language") ? json["Language"] ?? "" : "",
+        country: json.containsKey("Country") ? json["Country"] ?? "" : "",
+        awards: json.containsKey("Awards") ? json["Awards"] ?? "" : "",
+        ratings: json.containsKey("Ratings")
+            ? (json["Ratings"] as List).map((e) => Ratings.fromJson(e)).toList()
+            : [],
+        metascore: json.containsKey("Metascore") ? json["Metascore"] ?? "" : "",
+        imdbRating:
+            json.containsKey("imdbRating") ? json["imdbRating"] ?? "" : "",
+        imdbVotes: json.containsKey("imdbVotes") ? json["imdbVotes"] ?? "" : "",
+        dvd: json.containsKey("DVD") ? json["DVD"] ?? "" : "",
+        boxOffice: json.containsKey("BoxOffice") ? json["BoxOffice"] ?? "" : "",
+        production:
+            json.containsKey("Production") ? json["Production"] ?? "" : "",
+        website: json.containsKey("Website") ? json["Website"] ?? "" : "",
+        response: json.containsKey("Response") ? json["Response"] ?? "" : "",
       );
-      // year = json['Year'];
-      // rated = json['Rated'];
-      // released = json['Released'];
-      // runtime = json['Runtime'];
-      // genre = json['Genre'];
-      // director = json['Director'];
-      // writer = json['Writer'];
-      // actors = json['Actors'];
-      // plot = json['Plot'];
-      // language = json['Language'];
-      // country = json['Country'];
-      // awards = json['Awards'];
-      // poster = json['Poster'];
-      // if (json['Ratings'] != null) {
-      //   ratings = <Ratings>[];
-      //   json['Ratings'].forEach((v) {
-      //     ratings!.add(new Ratings.fromJson(v));
-      //   });
-      // }
-      // metascore = json['Metascore'];
-      // imdbRating = json['imdbRating'];
-      // imdbVotes = json['imdbVotes'];
-      // imdbID = json['imdbID'];
-      // type = json['Type'];
-      // dVD = json['DVD'];
-      // boxOffice = json['BoxOffice'];
-      // production = json['Production'];
-      // website = json['Website'];
-      // response = json['Response'];
     } catch (error) {
       return ProductionModel();
     }
